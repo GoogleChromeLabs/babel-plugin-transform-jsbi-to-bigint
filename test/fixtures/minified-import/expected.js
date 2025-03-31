f613f2c9b3713be8f1e21f965749be35fd1f2406
jakobkummerow <jakob.kummerow@gmail.com>
2025-03-31 10:41:20 +0200

    Fix "instanceof" when "JSBI" is minified (#18)
    This fixes the transformation of:
      x instanceof RenamedJSBI
    into
      typeof x === "bigint"
    when JSBI has been minified/renamed.
    
    Fixes: #17

---
+++ test/fixtures/minified-import/expected.js
@@ -0,0 +1,17 @@
+// Copyright 2025 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the “License”);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// <https://apache.org/licenses/LICENSE-2.0>.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+let num = 123n;
+let JSBI = String;
+console.log(typeof num === "bigint"); // Should be transformed.
+console.log(num instanceof JSBI); // Shouldn't be transformed.
